0a4fa6fab98ccfd67d03f02ad0200a4bf08a96f6
Paula Sierra <paula@MacBook-Pro-de-Paula.local>
2023-04-26 22:50:37 +0200

    “f”

--- src/partials/Features.jsx
+++ src/partials/Features.jsx
@@ -103,7 +103,7 @@ function Features() {
                                                         <a className="font-medium text-gray-600 hover:text-gray-900 px-5 py-3 flex items-center transition duration-150 ease-in-out" href="https://github.com/paulasierra7/Amazon-Copy-E-Commerce">View code</a>
                                                     </li>
                                                     <li>
-                                                        <a className="btn-sm text-gray-200 bg-gray-900 hover:bg-gray-800 ml-3" href="https://amazon-ecommerce-chi.vercel.app/">
+                                                        <a className="btn-sm text-gray-200 bg-gray-900 hover:bg-gray-800 ml-3" href="https://wheels-carpooling-app-7h89.vercel.app/">
                                                             <span>View website</span>
                                                             <svg className="w-3 h-3 fill-current text-gray-400 flex-shrink-0 ml-2 -mr-1" viewBox="0 0 12 12" xmlns="http://www.w3.org/2000/svg">
                                                                 <path d="M11.707 5.293L7 .586 5.586 2l3 3H0v2h8.586l-3 3L7 11.414l4.707-4.707a1 1 0 000-1.414z" fillRule="nonzero"/>
